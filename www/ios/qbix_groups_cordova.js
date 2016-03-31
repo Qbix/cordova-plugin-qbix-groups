ca475b82eeed23aca74466c0bc90999fa5a368f2
Igor <kvofreelance@gmail.com>
2016-04-01 01:58:56 +0300

    Added dependency cordoba-plugin-media

--- www/ios/qbix_groups_cordova.js
+++
@@ -1,8 +0,0 @@
-var exec = require('cordova/exec');
-var qbix_groups_cordova = require('../qbix_groups_cordova.js');
-
-qbix_groups_cordova.convertGroupsBinFileToJson = function(onSuccess, onError) {
-	exec(onSuccess,onError,'QGroups','convertGroupsBinFileToJson', []);
-}
-
-module.exports = qbix_groups_cordova;
--- www/qbix_groups_cordova.js
+++ www/qbix_groups_cordova.js
@@ -1,7 +1,10 @@
 module.exports = {
     hello: function (name, successCallback, errorCallback) {
         cordova.exec(successCallback, errorCallback, "QbixGroupsCordova", "hello", [name]);
-    }
+    },
+    convertGroupsBinFileToJson = function(onSuccess, onError) {
+		exec(onSuccess,onError,'QGroups','convertGroupsBinFileToJson', []);
+	}
 };
 
 
